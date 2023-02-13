13e60eb78aa04d476a136a615d2ed8ad9ad443ae
Evgeniy Malygin <black2001@inbox.ru>
2023-02-13 10:23:12 +0300

    Подключил БД, liquibase настроил

--- example-for-graduate-work-main/src/main/resources/liquibase/scripts/user-malygin.sql
+++ example-for-graduate-work-main/src/main/resources/liquibase/scripts/user-malygin.sql
@@ -2,12 +2,12 @@
 -- changeset emalygin:1
 CREATE TABLE users
 (
-    user_id          BIGINT          PRIMARY KEY,
+    user_id          INT          PRIMARY KEY,
     user_email       VARCHAR         NOT NULL,
-    user_firstName   VARCHAR         NOT NULL,
-    user_lastName    VARCHAR         NOT NULL,
+    user_first_name   VARCHAR         NOT NULL,
+    user_last_name    VARCHAR         NOT NULL,
     user_phone       VARCHAR         NOT NULL,
-    user_regDate     VARCHAR         NOT NULL,
+    user_reg_date     VARCHAR         NOT NULL,
     user_city        VARCHAR         NOT NULL,
     user_image       VARCHAR         NOT NULL
 );
@@ -27,7 +27,7 @@ CREATE TABLE comment
     comment_id          BIGINT          PRIMARY KEY,
     comment_author       int4         NOT NULL,
     comment_pk           int4         NOT NULL,
-    comment_createdAt       VARCHAR         NOT NULL,
+    comment_created_at       VARCHAR         NOT NULL,
     comment_text       VARCHAR         NOT NULL
 );
 
