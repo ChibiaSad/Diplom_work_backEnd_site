f79965a27e5a5bd5f77be27574345c34dd6f74d4
ChibiaSad <magur1997@gmail.com>
2023-02-21 15:28:44 +0300

    changed types

--- example-for-graduate-work-main/src/main/resources/liquibase/scripts/user-malygin.sql
+++ example-for-graduate-work-main/src/main/resources/liquibase/scripts/user-malygin.sql
@@ -2,19 +2,19 @@
 -- changeset alikeli:1
 CREATE TABLE users
 (
-    user_id         INT PRIMARY KEY,
+    user_id         SERIAL PRIMARY KEY,
     user_email      VARCHAR NOT NULL,
     user_first_name VARCHAR NOT NULL,
     user_last_name  VARCHAR NOT NULL,
     user_phone      VARCHAR NOT NULL,
-    user_reg_date   VARCHAR NOT NULL,
-    user_city       VARCHAR NOT NULL,
-    user_image      int8 NOT NULL
+    user_reg_date   VARCHAR,
+    user_city       VARCHAR,
+    user_image      int4
 );
 
 CREATE TABLE ads
 (
-    ads_id        int4 PRIMARY KEY,
+    ads_id        SERIAL PRIMARY KEY,
     ads_author_id INT REFERENCES users (user_id),
     ads_image     VARCHAR NOT NULL,
     ads_price     int4    NOT NULL,
@@ -24,7 +24,7 @@ CREATE TABLE ads
 
 CREATE TABLE comment
 (
-    comment_id         int4 PRIMARY KEY,
+    comment_id         SERIAL PRIMARY KEY,
     comment_author_id  int4    NOT NULL,
     comment_created_at VARCHAR NOT NULL,
     comment_text       VARCHAR NOT NULL,
@@ -34,7 +34,7 @@ CREATE TABLE comment
 
 CREATE TABLE avatar
 (
-    avatar_id   int8 PRIMARY KEY,
+    avatar_id   SERIAL PRIMARY KEY,
     avatar_path VARCHAR NOT NULL,
     user_id     INT REFERENCES users (user_id)
 
@@ -42,7 +42,7 @@ CREATE TABLE avatar
 
 CREATE TABLE image
 (
-    image_id   int4 PRIMARY KEY,
+    image_id   SERIAL PRIMARY KEY,
     image_path VARCHAR NOT NULL,
     ads_id     int4 REFERENCES ads (ads_id)
 );
