fe7f7fb9d3a1d82c9f09c8674a3c412ab6315782
EvgeniyMalygin <black2001@inbox.ru>
2023-02-25 12:30:00 +0300

    написаны тесты для класса CommentMapper

--- example-for-graduate-work-main/src/test/java/ru/skypro/homework/CommentMapperTest.java
+++ example-for-graduate-work-main/src/test/java/ru/skypro/homework/CommentMapperTest.java
@@ -2,18 +2,31 @@ package ru.skypro.homework;
 
 import org.junit.jupiter.api.Test;
 import ru.skypro.homework.dto.CommentDto;
+import ru.skypro.homework.dto.ResponseWrapperCommentDto;
 import ru.skypro.homework.entity.Comment;
+import ru.skypro.homework.entity.User;
 import ru.skypro.homework.mapper.CommentMapper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CommentMapperTest {
     @Test
     public void shouldMapCommentToCommentDto() {
+        User user = new User();
+        user.setId(1);
+        user.setEmail("user@gmail.com");
+        user.setPhone("+78005553535");
+        user.setFirstName("First");
+        user.setLastName("Last");
+
         Comment comment = new Comment();
         comment.setId(1);
         comment.setCreatedAt("create");
         comment.setText("Text");
+        comment.setAuthor(user);
 
         CommentDto commentDto = CommentMapper.INSTANCE.commentToCommentDto(comment);
 
@@ -24,19 +37,31 @@ public class CommentMapperTest {
 
     }
 
-//    @Test
-//    public void shouldMapCommentDtoToComment() {
-//        CommentDto commentDto = new CommentDto();
-//        commentDto.setAuthor(1);
-//        commentDto.setCreatedAt("create");
-//        commentDto.setText("Text");
-//
-//        Comment comment = CommentMapper.INSTANCE.c(commentDto);
-//
-//        assertThat(comment).isNotNull();
-//        assertThat(comment.getId()).isEqualTo(1);
-//        assertThat(comment.getCreatedAt()).isEqualTo("create");
-//        assertThat(comment.getText()).isEqualTo("Text");
-//    }
+    @Test
+    public void shouldMapCommentDtoToWrapperCommentDto(){
+
+        CommentDto comment = new CommentDto();
+        comment.setAuthor(1);
+        comment.setCreatedAt("create");
+        comment.setText("Text");
+        comment.setPk(1);
+
+        CommentDto comment1 = new CommentDto();
+        comment1.setAuthor(2);
+        comment1.setCreatedAt("create1");
+        comment1.setText("Text1");
+        comment1.setPk(3);
+
+        List<CommentDto> dtoList = new ArrayList<>();
+        dtoList.add(comment);
+        dtoList.add(comment1);
+
+        ResponseWrapperCommentDto respDto = new ResponseWrapperCommentDto();
+        respDto.setCount(dtoList.size());
+        respDto.setResults(dtoList);
+
+        assertThat(CommentMapper.INSTANCE.CommentDtoToWrapperCommentDto(dtoList, dtoList.size())).isEqualTo(respDto);
+    }
+
 }
 
