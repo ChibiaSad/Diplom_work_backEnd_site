1031edc1d6ca6c2cd1219560d02504ac9363136d
EvgeniyMalygin <black2001@inbox.ru>
2023-02-25 12:39:02 +0300

    написаны тесты для класса UserMapper

--- example-for-graduate-work-main/src/test/java/ru/skypro/homework/UserMapperTest.java
+++ example-for-graduate-work-main/src/test/java/ru/skypro/homework/UserMapperTest.java
@@ -2,59 +2,41 @@ package ru.skypro.homework;
 
 import org.junit.jupiter.api.Test;
 import ru.skypro.homework.dto.UserDto;
+import ru.skypro.homework.entity.Avatar;
 import ru.skypro.homework.entity.User;
 import ru.skypro.homework.mapper.UserMapper;
+
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class UserMapperTest {
-//    @Test
-//    public void shouldMapUserToUserDto() {
-//        User user = new User();
-//        user.setId(7);
-//        user.setEmail("user@gmail.com");
-//        user.setFirstName("Ivan");
-//        user.setLastName("Ivanov");
-//        user.setPhone("89077777777");
-//        user.setRegDate("15/02/2023");
-//        user.setCity("City");
-//        user.setImage("image.png");
-//
-//        UserDto userDto = UserMapper.INSTANCE.userToUserDto(user);
-//
-//        assertThat(userDto).isNotNull();
-//        assertThat(userDto.getId()).isEqualTo(7);
-//        assertThat(userDto.getEmail()).isEqualTo("user@gmail.com");
-//        assertThat(userDto.getFirstName()).isEqualTo("Ivan");
-//        assertThat(userDto.getLastName()).isEqualTo("Ivanov");
-//        assertThat(userDto.getPhone()).isEqualTo("89077777777");
-//        assertThat(userDto.getRegDate()).isEqualTo("15/02/2023");
-//        assertThat(userDto.getCity()).isEqualTo("City");
-//        assertThat(userDto.getImage()).isEqualTo("image.png");
-//    }
-//
-//    @Test
-//    public void shouldMapUserDtoToUser() {
-//        UserDto userDto = new UserDto();
-//        userDto.setId(7);
-//        userDto.setEmail("user@gmail.com");
-//        userDto.setFirstName("Ivan");
-//        userDto.setLastName("Ivanov");
-//        userDto.setPhone("89077777777");
-//        userDto.setRegDate("15/02/2023");
-//        userDto.setCity("City");
-//        userDto.setImage("image.png");
-//
-//        User user = UserMapper.INSTANCE.userDtoToUser(userDto);
-//
-//        assertThat(user).isNotNull();
-//        assertThat(user.getId()).isEqualTo(7);
-//        assertThat(user.getEmail()).isEqualTo("user@gmail.com");
-//        assertThat(user.getFirstName()).isEqualTo("Ivan");
-//        assertThat(user.getLastName()).isEqualTo("Ivanov");
-//        assertThat(user.getPhone()).isEqualTo("89077777777");
-//        assertThat(user.getRegDate()).isEqualTo("15/02/2023");
-//        assertThat(user.getCity()).isEqualTo("City");
-//        assertThat(user.getImage()).isEqualTo("image.png");
-//    }
+    @Test
+    public void shouldMapUserToUserDto() {
+
+        User user = new User();
+        user.setId(1);
+        user.setEmail("user@gmail.com");
+        user.setPhone("+78005553535");
+        user.setFirstName("First");
+        user.setLastName("Last");
+
+        Avatar avatar = new Avatar();
+        avatar.setUser(user);
+        avatar.setFilePath("src/test/java/ru/skypro/homework/resources/2_2023-02-22.jpg");
+        avatar.setId(1);
+
+        user.setAvatar(avatar);
+
+        UserDto userDto = UserMapper.INSTANCE.userToUserDto(user);
+
+        assertThat(userDto).isNotNull();
+        assertThat(userDto.getId()).isEqualTo(user.getId());
+        assertThat(userDto.getEmail()).isEqualTo(user.getEmail());
+        assertThat(userDto.getFirstName()).isEqualTo(user.getFirstName());
+        assertThat(userDto.getLastName()).isEqualTo(user.getLastName());
+        assertThat(userDto.getPhone()).isEqualTo(user.getPhone());
+        assertThat(userDto.getImage()).isEqualTo("/users/me/image/" + avatar.getId());
+    }
+
+
 }
