1824ffed6f9061585d124b770f1c83e4a345a525
ChibiaSad <magur1997@gmail.com>
2023-03-03 18:12:15 +0300

    подправлен default user

--- example-for-graduate-work-main/src/main/resources/liquibase/scripts/user-malygin.sql
+++ example-for-graduate-work-main/src/main/resources/liquibase/scripts/user-malygin.sql
@@ -66,3 +66,8 @@ ALTER TABLE users ADD COLUMN user_role varchar NOT NULL ;
 
 INSERT INTO users (user_email, user_first_name, user_last_name, user_phone, user_password, user_image, user_role)
 values ('user@gmail.com', 'First', 'Last', '+78005553535', '{bcrypt}$2a$10$EryGOpG7y4NSHVMtDSjPkuOazwkd4HHnLLBJ.83hu1UC0W9xlib3K', null, 'USER');
+
+--changeset chibiaSad:6
+UPDATE users SET user_password = '{bcrypt}$2a$10$oGoagZ.IxETOFmTtk0l27.ov9IG0NILUWbTRGSKqyCMb3OFZk14Tm',
+                 user_role = 'ADMIN'
+WHERE user_email = 'user@gmail.com';
